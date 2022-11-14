dc960d3b14e2e94eaaaa3272d49a0ab92a43463d
[konstantinos kalaitzis] <konstantinoskalaitzis@yahoo.com>
2022-11-14 12:19:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Profile from "./Profile";
 
-function App() {
+
+
+export default function App() {
+  
+  const myFunction = (myName) => {
+    alert(` ${myName} is a student he is lying `);
+  
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profile Fullname="John Doe" myFunction={myFunction} Bio="owner of seven software engineering companies" Proffession="he is lying"/>
+          < img src="cat.jpg"/>
+         
     </div>
+    
   );
 }
 
-export default App;
+
